95eb0a2aa3e7fefd2cc05ae67cf7e6fe400923c0
Jet Conway <jconway@bsd220.org>
2020-01-28 10:42:52 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Jet Conway on 1/28/20.
+//  Copyright © 2020 Jet Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
